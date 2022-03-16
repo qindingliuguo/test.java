ec4e0386b3251baebddb15d5db3b574559590ce2
秦定六国 <1312941775@qq.com>
2022-03-16 19:12:54 +0800

    Create Vartest02.java

---
+++ Vartest02.java
@@ -0,0 +1,15 @@
+import javax.crypto.interfaces.PBEKey;
+
+public class Vartest02 {
+                     static int[] a = {1,2,3,4,5,6,7};
+    public int i;
+    public Vartest02(int p){
+        this.i = p;
+    }
+    public static void main(String[]args){
+        for(int i:a){
+            System.out.println(i);
+        }
+    }
+
+}
