ed5552c57d9991db3f667aa5839ab030d8d5cb32
秦定六国 <1312941775@qq.com>
2022-03-16 19:13:55 +0800

    Create 方法01.java

---
+++ 方法01.java
@@ -0,0 +1,11 @@
+public class 方法01 {
+    public static int  sumint(int a,int b){
+        int c = a + b;
+        System.out.println(c);
+        return c ;
+    }
+    public static void main(String[]args){
+        方法01.sumint(210,20);
+    }
+
+}
