ebd6473047b78f2c3c6c8d0c1a8e727e3ad29305
秦定六国 <1312941775@qq.com>
2022-03-16 19:13:25 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+class A
+{
+    public static void main(String[] args){
+        System.out.println("asd");
+    }
+}
+class B
+{
+    public static void main(String[] args){
+        System.out.println("zxc");
+    }
+}
+public class test
+{
+    public static void main(String[] arge){
+        System.out.println("qwe");
+    }
+}
