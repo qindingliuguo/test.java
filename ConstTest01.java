42fa72887a9e4f8be7091c4539083da8f7a1b822
秦定六国 <1312941775@qq.com>
2022-03-16 19:11:32 +0800

    Create ConstTest01.java

---
+++ ConstTest01.java
@@ -0,0 +1,11 @@
+public class ConstTest01 {
+    public static void main(String[]arge){
+        System.out.println("asd");
+        System.out.println("你好");
+        System.out.println('b');
+        System.out.println(10);
+        System.out.println(false);
+        System.out.println(true);
+        System.out.println(3.14);
+    }
+}
